31d2db8ce0c4d68711c4fe02e3727dec782edef1
czkmt <czkmt>
1999-12-04 15:56:09 +0000

    (isearch-repeat): New advice.

--- skk-isearch.el
+++ skk-isearch.el
@@ -5,9 +5,9 @@
 
 ;; Author: Enami Tsugutomo <enami@ba2.so-net.or.jp>
 ;; Maintainer: Mikio Nakajima <minakaji@osaka.email.ne.jp>
-;; Version: $Id: skk-isearch.el,v 1.5.2.4.2.7 1999/11/28 15:10:32 czkmt Exp $
+;; Version: $Id: skk-isearch.el,v 1.5.2.4.2.8 1999/12/04 15:56:09 czkmt Exp $
 ;; Keywords: japanese
-;; Last Modified: $Date: 1999/11/28 15:10:32 $
+;; Last Modified: $Date: 1999/12/04 15:56:09 $
 
 ;; This file is part of Daredevil SKK.
 
@@ -270,7 +270,7 @@ Optional argument PREFIX is apppended if given."
   ;; Keys for `skk-isearch-delete-char'.
   (let ((commands '(backward-delete-char-untabify
 		    backward-delete-char
-		    backward-or-forward-delete-char 
+		    backward-or-forward-delete-char
 		    delete-backward-char
 		    ))
 	keys )
@@ -504,6 +504,57 @@ If the current mode is different from previous, remove it first."
 	  ;; restore the overriding local map.
 	  (set-buffer current-buffer)
 	  (set skk-isearch-overriding-local-map local-map) )))))
+
+;;
+;; Advices.
+;;
+
+(defvar isearch-just-started nil)
+(defadvice isearch-repeat (around skk-isearch-ad activate)
+  ;; Utility for isearch-repeat-forward and -backward.
+  (if (eq isearch-forward (eq direction 'forward))
+      ;; C-s in forward or C-r in reverse.
+      (if (equal isearch-string "")
+	  ;; If search string is empty, use last one.
+	  (setq isearch-string
+		(or (if isearch-regexp
+			(car regexp-search-ring)
+		      (car search-ring))
+		    "")
+		isearch-message
+		(concat (skk-isearch-mode-string)
+			(mapconcat 'isearch-text-char-description
+				   isearch-string "")))
+	;; If already have what to search for, repeat it.
+	(or isearch-success
+	    (progn
+	      (goto-char (if isearch-forward (point-min) (point-max)))
+	      (setq isearch-wrapped t))))
+    ;; C-s in reverse or C-r in forward, change direction.
+    (setq isearch-forward (not isearch-forward)))
+
+  (setq isearch-barrier (point)) ; For subsequent \| if regexp.
+
+  (if (equal isearch-string "")
+      (setq isearch-success t)
+    (if (and isearch-success (equal (match-end 0) (match-beginning 0))
+	     (not isearch-just-started))
+	;; If repeating a search that found
+	;; an empty string, ensure we advance.
+	(if (if isearch-forward (eobp) (bobp))
+	    ;; If there's nowhere to advance to, fail (and wrap next time).
+	    (progn
+	      (setq isearch-success nil)
+	      (and executing-kbd-macro
+		   (not defining-kbd-macro)
+		   (isearch-done))
+	      (ding nil 'isearch-failed))
+	  (forward-char (if isearch-forward 1 -1))
+	  (isearch-search))
+      (isearch-search)))
+
+  (isearch-push-state)
+  (isearch-update))
 
 (put 'skk-isearch-wrapper 'isearch-command t)
 (put 'skk-isearch-keyboard-quit 'isearch-command t)
